fac779fc20eb2838bc0882e65e5f0a12081dd001
kdo_1999/mac <kdo_1999@naver.com>
2024-12-02 15:31:06 +0900

    feat: Sql.append, Spl.insert 추가

--- src/main/java/com/sql/Sql.java
+++ src/main/java/com/sql/Sql.java
@@ -1,4 +1,34 @@
 package com.sql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.simpleDb.SimpleDb;
+
 public class Sql {
+	private StringBuilder sb = new StringBuilder();
+	private List<Object> params = new ArrayList<>();
+	private final SimpleDb simpleDb;
+
+	public Sql(SimpleDb simpleDb) {
+		this.simpleDb = simpleDb;
+	}
+
+	public Sql append(String sql, Object... params) {
+		sb.append(sql + " ");
+		Collections.addAll(this.params, params);
+
+		return this;
+	}
+
+	public long insert() {
+		long id = simpleDb.runInsert(sb.toString(), params);
+		System.out.println("id = " + id);
+
+		return id;
+	}
 }
