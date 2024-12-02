35ac4f7d1b95d640f91538d05dc25ef6e50cc180
류대현 <eogus1027@naver.com>
2024-12-02 17:19:24 +0900

    테스트 데이터를 DB에 추가하는 기능 구현

---
+++ src/main/java/org/example/SimpleDb.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.sql.*;
+
+public class SimpleDb {
+    private final int port = 3306;
+    private boolean devMode;
+    private Connection conn;
+
+    public SimpleDb(String host, String user, String password, String name){
+        String url = "jdbc:mysql://"+ host + ":" + port + "/" + name;
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDevMode(boolean isDevMode) {
+        this.devMode = isDevMode;
+    }
+
+    public void run(String sql) {
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(String sql, String title, String body, boolean isBlind) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, title);
+            pstmt.setString(2, body);
+            pstmt.setBoolean(3, isBlind);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
