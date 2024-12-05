2ec668c9bff7dc868db950f9d9c16a96a27f7b98
kdo_1999/mac <kdo_1999@naver.com>
2024-12-05 12:19:59 +0900

    refactor: 중복코드 분리, 주석 추가

--- src/main/java/com/simpleDb/SimpleDb.java
+++ src/main/java/com/simpleDb/SimpleDb.java
@@ -1,6 +1,5 @@
 package com.simpleDb;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,12 +9,9 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
-import javax.sql.DataSource;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
@@ -52,7 +48,12 @@ public class SimpleDb {
 
 	private void printSql(PreparedStatement preparedStatement) {
 		if (devMode) {
-			System.out.println(preparedStatement.toString());
+			String preparedStatementString = preparedStatement.toString();
+			String sql = preparedStatementString
+				.substring(preparedStatementString.indexOf(':') + 2, preparedStatementString.length());
+
+			System.out.println("== rawSql ==");
+			System.out.println(sql);
 		}
 	}
 
@@ -81,12 +82,25 @@ public class SimpleDb {
 		return new Sql(this);
 	}
 
+	/**
+	 * 데이터 추가 메소드
+	 * @param sql SQL
+	 * @param params 파라미터 값
+	 * @return {@link Long}
+	 */
 	public long runInsert(String sql, List<Object> params) {
 		long id = excute(sql, params, PreparedStatement.RETURN_GENERATED_KEYS);
 
 		return id;
 	}
 
+	/**
+	 * 실제 DB에 결과 값을 가져오는 메소드
+	 * @param sql SQL
+	 * @param params 파라미터 값
+	 * @param generatedKey 리턴 값
+	 * @return {@link Long}
+	 */
 	private long excute(String sql, List<Object> params, int generatedKey) {
 		try {
 			Connection connection = getConnection();
@@ -115,13 +129,12 @@ public class SimpleDb {
 			throw new RuntimeException(e);
 		}
 	}
-
-	private void setParams(List<Object> params, PreparedStatement preparedStatement) throws SQLException {
-		for (int i = 0; i < params.size(); i++) {
-			preparedStatement.setObject(i + 1, params.get(i));
-		}
-	}
-
+	/**
+	 * 데이터 수정 메소드
+	 * @param sql SQL
+	 * @param params 파라미터 값
+	 * @return {@link Long}
+	 */
 	public long runUpdate(String sql, List<Object> params) {
 		return excute(sql, params, PreparedStatement.NO_GENERATED_KEYS);
 	}
@@ -146,16 +159,28 @@ public class SimpleDb {
 		}
 	}
 
+	/**
+	 * 한개 데이터 불러오는 메소드
+	 * @param sql SQL
+	 * @param clazz 변환할 Class
+	 * @return {@link T} 파라미터로 들어온 Class로 반환
+ 	 */
 	public <T> T runSelectRow(String sql, Class<T> clazz) {
 		T result = null;
 		List<T> t = runSelectRows(sql, clazz);
 		if (t.isEmpty()) {
-			throw new RuntimeException();
+			throw new RuntimeException("해당 데이터가 존재하지 않습니다.");
 		}
 		result = t.get(0);
 		return result;
 	}
 
+	/**
+	 * 여러개의 데이터를 불러오는 메소드
+	 * @param sql SQL
+	 * @param clazz 변환할 Class
+	 * @return {@link List<T>} 파라미터로 들어온 Class를 List에 담아서 반환
+ 	 */
 	public <T> List<T> runSelectRows(String sql, Class<T> clazz) {
 		List<T> mapList = new ArrayList<>();
 		try {
@@ -166,11 +191,14 @@ public class SimpleDb {
 		return mapList;
 	}
 
+	/**
+	 * DB에서 데이터를 가져와서 List로 변한하는 메소드
+	 * @param sql SQL
+	 * @param clazz 변환할 Class
+	 * @return {@link T} 파라미터로 들어온 Class로 반한
+ 	 */
 	private <T> List<T> excuteSelect(String sql, Class<T> clazz) throws SQLException {
-		Connection connection = getConnection();
-		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-		ResultSet resultSet = preparedStatement.executeQuery();
+		ResultSet resultSet = getResultSet(sql);
 		ResultSetMetaData metaData = resultSet.getMetaData();
 
 		List<T> list = new LinkedList<>();
@@ -185,13 +213,15 @@ public class SimpleDb {
 		return list;
 	}
 
+	/**
+	 * DB에서 값을 가져와서 데이터 가공후 반한하는 메소드
+	 * @param sql SQL
+	 * @param params 파라미터 값
+	 * @return {@link List}
+	 * @throws SQLException
+	 */
 	private List<Map<String, Object>> excuteSelect(String sql, List<Object> params) throws SQLException {
-		Connection connection = getConnection();
-		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-		setParams(params, preparedStatement);
-
-		ResultSet resultSet = preparedStatement.executeQuery();
+		ResultSet resultSet = getResultSet(sql, params);
 		ResultSetMetaData metaData = resultSet.getMetaData();
 
 		List<Map<String, Object>> mapList = new LinkedList<>();
@@ -209,6 +239,12 @@ public class SimpleDb {
 		return mapList;
 	}
 
+	/**
+	 * DB기준 현재 시간을 가져오는 메소드
+	 * @param sql SQL
+	 * @param params 파라미터 값
+	 * @return {@link LocalDateTime}
+ 	 */
 	public LocalDateTime runSelectDatetime(String sql, List<Object> params) {
 		Map<String, Object> result = runSelectRow(sql, params);
 
@@ -216,6 +252,12 @@ public class SimpleDb {
 			LocalDateTime.parse(result.get("NOW()").toString()) : null;
 	}
 
+	/**
+	 * 결과 값이 숫자인 데이터 가져오는 메소드
+	 * @param sql
+	 * @param params
+	 * @return {@link Long}
+	 */
 	public Long runSelectLong(String sql, List<Object> params) {
 		Map<String, Object> result = runSelectRow(sql, params);
 
@@ -228,6 +270,12 @@ public class SimpleDb {
 		return Long.parseLong(result.get(split[1]).toString());
 	}
 
+	/**
+	 * 결과 값이 String인 데이터를 가져오는 메소드
+	 * @param sql
+	 * @param params
+	 * @return {@link String}
+	 */
 	public String runSelectString(String sql, List<Object> params) {
 		Map<String, Object> result = runSelectRow(sql, params);
 
@@ -240,26 +288,21 @@ public class SimpleDb {
 		return (String)result.get(split[1]);
 	}
 
+	/**
+	 * 결과 값이 Boolean인 데이터를 가져오는 메소드
+	 * @param sql
+	 * @param params
+	 * @return {@link Boolean}
+	 */
 	public Boolean runSelectBoolean(String sql, List<Object> params) {
 		Map<String, Object> result = runSelectRow(sql, params);
-		StringBuilder sb = new StringBuilder();
+		String key = getKey(sql);
 
-		String[] split = sql.split(" ");
-
-		for (int i = 1; i < split.length; i++) {
-			if (split[i].equals("FROM")) {
-				break;
-			}
-			sb.append(split[i] + " ");
-		}
-
-		sb.deleteCharAt(sb.length() - 1);
-
-		if (!result.containsKey(sb.toString())) {
+		if (!result.containsKey(key)) {
 			throw new RuntimeException("해당 테이터가 존재하지 않습니다.");
 		}
 
-		Object obj = result.get(sb.toString());
+		Object obj = result.get(key);
 
 		Boolean booleanValue;
 
@@ -267,7 +310,7 @@ public class SimpleDb {
 			case Boolean b -> booleanValue = (Boolean)obj;
 			case String s -> booleanValue = "true".equals(obj.toString().toLowerCase());
 			case Number n -> {
-				Number number = (Number)result.get(sb.toString());
+				Number number = (Number)result.get(key);
 
 				booleanValue = number.intValue() == 1;
 			}
@@ -277,22 +320,16 @@ public class SimpleDb {
 		return booleanValue;
 	}
 
+	/**
+	 * 결과 값이 Long인 데이터 여러개를 가져오는 메소드
+	 * @param sql
+	 * @param params
+	 * @return {@link List<Long>}
+	 */
 	public List<Long> runSelectLongs(String sql, List<Object> params) {
 		List<Map<String, Object>> mapList = runSelectRows(sql, params);
 
-		StringBuilder sb = new StringBuilder();
-
-		String[] split = sql.split(" ");
-
-		for (int i = 1; i < split.length; i++) {
-			if (split[i].equals("FROM")) {
-				break;
-			}
-			sb.append(split[i] + " ");
-		}
-
-		sb.deleteCharAt(sb.length() - 1);
-		String key = sb.toString();
+		String key = getKey(sql);
 
 		List<Long> result = new LinkedList<>();
 
@@ -309,6 +346,73 @@ public class SimpleDb {
 		return result;
 	}
 
+	/**
+	 * preparedStatement에 파라미터 바인딩하는 메소드
+	 * @param params
+	 * @param preparedStatement
+	 * @throws SQLException
+	 */
+	private void setParams(List<Object> params, PreparedStatement preparedStatement) throws SQLException {
+		for (int i = 0; i < params.size(); i++) {
+			preparedStatement.setObject(i + 1, params.get(i));
+		}
+	}
+
+	/**
+	 * 쿼리를 DB에 보내고 결과 값을 반환해주는 메소드
+	 * @param sql
+	 * @return {@link ResultSet}
+	 * @throws SQLException
+	 */
+	private ResultSet getResultSet(String sql) throws SQLException {
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		printSql(preparedStatement);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		return resultSet;
+	}
+
+	/**
+	 * 쿼리를 DB에 보내고 결과 값을 반환해주는 메소드
+	 * @param sql
+	 * @param params
+	 * @return
+	 * @throws SQLException
+	 */
+	private ResultSet getResultSet(String sql, List<Object> params) throws SQLException {
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		setParams(params, preparedStatement);
+		printSql(preparedStatement);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		return resultSet;
+	}
+
+	/**
+	 * sql에 SELECT한 컬럼명 가져오는 메소드
+	 * @param sql
+	 * @return {@link String}
+	 */
+	private String getKey(String sql) {
+		StringBuilder sb = new StringBuilder();
+
+		String[] split = sql.split(" ");
+
+		for (int i = 1; i < split.length; i++) {
+			if (split[i].equals("FROM")) {
+				break;
+			}
+			sb.append(split[i] + " ");
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	/**
+	 * 사용한 Connection을 close하는 메소드
+	 * @throws RuntimeException
+	 */
 	public void closeConnection() {
 		Connection connection = threadLocal.get();
 		try {
@@ -320,6 +424,9 @@ public class SimpleDb {
 		}
 	}
 
+	/**
+	 * 트랜잭션 시작 메소드
+	 */
 	public void startTransaction() {
 		Connection connection = getConnection();
 		try {
@@ -329,6 +436,9 @@ public class SimpleDb {
 		}
 	}
 
+	/**
+	 * 롤백 메소드
+	 */
 	public void rollback() {
 		Connection connection = getConnection();
 		try {
@@ -338,6 +448,9 @@ public class SimpleDb {
 		}
 	}
 
+	/**
+	 * 커밋 메소드
+	 */
 	public void commit() {
 		Connection connection = getConnection();
 		try {
